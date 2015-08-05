8feaf3ae803d50986f962898148ea4ea3e3cec08
HyukSoa <leesoa@qq.com>
2015-08-06 01:09:42 +0800

    listview显示了然而丑

--- app/src/main/java/edu/zju/realmofmist/app/RankingActivityFragment.java
+++ app/src/main/java/edu/zju/realmofmist/app/RankingActivityFragment.java
@@ -24,7 +24,12 @@ public class RankingActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ranking, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_ranking, container, false);
+
+        linkGUI2Val(rootView);
+        populateList();
+
+        return rootView;
     }
 
 
