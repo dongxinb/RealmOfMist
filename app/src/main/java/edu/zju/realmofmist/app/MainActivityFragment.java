18ba82dece62169abed9c570d04245cb95afed37
dongxinb <dongxinb@gmail.com>
2015-08-05 23:26:36 +0800

    update

--- app/src/main/java/edu/zju/realmofmist/app/MainActivityFragment.java
+++ app/src/main/java/edu/zju/realmofmist/app/MainActivityFragment.java
@@ -2,9 +2,13 @@ package edu.zju.realmofmist.app;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import edu.zju.realmofmist.R;
 
@@ -13,12 +17,37 @@ import edu.zju.realmofmist.R;
  */
 public class MainActivityFragment extends Fragment {
 
+    FloatingActionsMenu mMenu;
+    FloatingActionButton mMenuProfile;
+    FloatingActionButton mMenuRanking;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        linkView(view);
+        return view;
+    }
+
+    private void linkView(View view) {
+        mMenu = (FloatingActionsMenu)view.findViewById(R.id.menu_actions);
+        mMenuProfile = (FloatingActionButton)view.findViewById(R.id.menu_profile);
+        mMenuRanking = (FloatingActionButton)view.findViewById(R.id.menu_ranking);
+
+        View.OnClickListener menuItemListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == mMenuProfile) {
+                    Log.d("Main", "Profile menu pressed.");
+                }else if (v == mMenuRanking) {
+                    Log.d("Main", "Ranking menu pressed.");
+                }
+            }
+        };
+        mMenuProfile.setOnClickListener(menuItemListener);
+        mMenuRanking.setOnClickListener(menuItemListener);
     }
 }
