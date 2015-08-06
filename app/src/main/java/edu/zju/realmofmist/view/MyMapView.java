c97694bd8cc1535890f742054b8b4649f7e4b095
dongxinb <dongxinb@gmail.com>
2015-08-06 11:02:47 +0800

    test circle

--- app/src/main/java/edu/zju/realmofmist/view/MyMapView.java
+++ app/src/main/java/edu/zju/realmofmist/view/MyMapView.java
@@ -1,6 +1,11 @@
 package edu.zju.realmofmist.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -25,5 +30,19 @@ public class MyMapView extends MapView {
 
     public MyMapView(Context context, GoogleMapOptions options) {
         super(context, options);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Canvas canvas1 = new Canvas();
+
+        Paint clearAwayPaint = new Paint();
+        clearAwayPaint.setStyle(Paint.Style.FILL);
+        clearAwayPaint.setColor(Color.BLACK);
+        clearAwayPaint.setAlpha(255);
+        clearAwayPaint.setShader(new RadialGradient(10, 10, 10, Color.BLACK, Color.TRANSPARENT, Shader.TileMode.MIRROR));
+
+        canvas.drawCircle(10, 10, 10, clearAwayPaint);
     }
 }
