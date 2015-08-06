d414a9a0b7c9b98338c6731ee990cbbff6ededab
HyukSoa <leesoa@qq.com>
2015-08-06 09:27:44 +0800

    Revert 9d4924e..3900f67
    This rolls back to commit 9d4924e5ba2d745be1f91c878e5637a6a77e65f0.

--- app/src/main/java/edu/zju/realmofmist/app/MainActivity.java
+++ app/src/main/java/edu/zju/realmofmist/app/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.zju.realmofmist.app;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,20 +12,76 @@ import android.view.View;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 import edu.zju.realmofmist.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     FloatingActionsMenu mMenu;
     FloatingActionButton mMenuProfile;
     FloatingActionButton mMenuRanking;
+
+    private MapFragment mMapFragment;
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mRequestingLocationUpdates = true;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private LocationRequest mLocationRequest;
+
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         linkView();
+
+        setMapFragment();
+        mMapFragment.getMapAsync(this);
+        buildGoogleApiClient();
+        createLocationRequest();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {  // more about this later
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
     }
 
     private void linkView() {
@@ -41,10 +99,57 @@ public class MainActivity extends AppCompatActivity {
                     startActivityForResult(intent, 1000);
                     Log.d("Main", "Ranking menu pressed.");
                 }
+                mMenu.toggle();
             }
         };
         mMenuProfile.setOnClickListener(menuItemListener);
         mMenuRanking.setOnClickListener(menuItemListener);
+    }
+
+    // set up fragment
+    private void setMapFragment() {
+//        mMapFragment = (MapFragment) getFragmentManager()
+//                .findFragmentById(R.id.map);
+
+        GoogleMapOptions options = new GoogleMapOptions();
+        options.mapType(GoogleMap.MAP_TYPE_HYBRID)
+                .mapToolbarEnabled(true)
+                .rotateGesturesEnabled(false)
+                .tiltGesturesEnabled(false);
+//        mMapFragment = MapFragment.newInstance(options);
+        mMapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map_fragment);
+
+//        FragmentTransaction fragmentTransaction =
+//                getFragmentManager().beginTransaction();
+//        fragmentTransaction.add(R.id.mainContainer, mMapFragment);
+//        fragmentTransaction.commit();
+    }
+
+    // set up google play service
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    // init location request
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
     }
 
 
@@ -68,5 +173,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+        Log.d("MyDebug", "La: " + mCurrentLocation.getLatitude() + " Lo: " + mCurrentLocation.getLongitude() + " TIME: " + mLastUpdateTime);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMyLocationEnabled(true);
     }
 }
