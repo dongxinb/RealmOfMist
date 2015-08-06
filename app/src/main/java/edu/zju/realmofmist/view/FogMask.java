85b9ba94da315930cf558ef0c358fcba34d7144c
dongxinb <dongxinb@gmail.com>
2015-08-06 13:58:30 +0800

    update

--- app/src/main/java/edu/zju/realmofmist/view/FogMask.java
+++ app/src/main/java/edu/zju/realmofmist/view/FogMask.java
@@ -1,25 +1,44 @@
 package edu.zju.realmofmist.view;
 
+import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
-import android.view.Surface;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 /**
  * Created by desolate on 15/8/6.
  */
-public class FogMask extends Surface implements SurfaceHolder.Callback {
+public class FogMask extends SurfaceView implements SurfaceHolder.Callback {
 
 
-    public FogMask(SurfaceTexture surfaceTexture) {
-        super(surfaceTexture);
+    public FogMask(Context context) {
+        super(context);
+        setZOrderOnTop(true);
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        Log.d("FogMask", "context");
     }
 
+    public FogMask(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setZOrderOnTop(true);
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        getHolder().addCallback(this);
+        setFocusable(true);
+        Log.d("FogMask", "context attr");
+    }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        doDraw();
     }
 
     @Override
@@ -30,5 +49,51 @@ public class FogMask extends Surface implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.d("FogMask", "onDraw()");
+//        Paint paint = new Paint();
+//        paint.setStyle(Paint.Style.FILL);
+//        paint.setColor(0xffbbbbbb);
+//        paint.setAlpha(150);
+//        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+
+//        Paint mPaint = new Paint();
+//        mPaint.setAlpha(0);
+//        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+//        mPaint.setAntiAlias(true);
+//        canvas.drawCircle(10, 10, 40, mPaint);
+    }
+
+    private void doDraw() {
+        Canvas canvas=getHolder().lockCanvas();
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(0xffbbbbbb);
+        paint.setAlpha(150);
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+
+        Paint mPaint = new Paint();
+        mPaint.setAlpha(0);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        mPaint.setAntiAlias(true);
+        Path path = new Path();
+        for (int i = 0; i < 100; i ++) {
+            path.addCircle(100+i*2, 100 + i, 20, Path.Direction.CW);
+        }
+        for (int i = 0; i < 100; i ++) {
+            path.addCircle(400+i*2, 200 + i * 3, 10, Path.Direction.CW);
+        }
+//        path.moveTo(100, 100);
+//        path.arcTo(100, 100, 200, 100, (float)Math.PI / 6, (float)Math.PI * 5/ 6, true);
+        path.addCircle(300, 300, 40, Path.Direction.CW);
+        canvas.drawPath(path, mPaint);
+
+        getHolder().unlockCanvasAndPost(canvas);
+        Log.d("FogMask", "doDraw()");
+        invalidate();
     }
 }
--- app/src/main/java/edu/zju/realmofmist/view/MyMapView.java
+++ app/src/main/java/edu/zju/realmofmist/view/MyMapView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RadialGradient;
 import android.graphics.Shader;
 import android.util.AttributeSet;
@@ -49,14 +52,44 @@ public class MyMapView extends MapView {
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
 
-        Paint clearAwayPaint = new Paint();
-        clearAwayPaint.setStyle(Paint.Style.FILL);
-        clearAwayPaint.setColor(0xffbbbbbb);
-        clearAwayPaint.setAlpha(150);
-        clearAwayPaint.setShader(new )
-        //clearAwayPaint.setShader(new RadialGradient(100, 100, 100, Color.BLACK, Color.TRANSPARENT, Shader.TileMode.MIRROR));
-
-        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), clearAwayPaint);
-        Log.d("MapView", "onDraw()");
+//        Canvas canvas1 = new Canvas();
+//
+//        Paint paint = new Paint();
+//        paint.setStyle(Paint.Style.FILL);
+//        paint.setColor(0xffbbbbbb);
+//        paint.setAlpha(150);
+//        //clearAwayPaint.setShader(new RadialGradient(100, 100, 100, Color.BLACK, Color.TRANSPARENT, Shader.TileMode.MIRROR));
+//
+//        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+//
+////        for (int i = 0; i < 10; i ++) {
+////            Paint point = new Paint();
+////            point.setStyle(Paint.Style.FILL);
+////            point.setColor(Color.BLACK);
+////            point.setAlpha(255);
+////            point.setShader(new RadialGradient(100 * 25 * i, 100, 25, Color.TRANSPARENT, Color.TRANSPARENT, Shader.TileMode.MIRROR));
+//////            canvas.drawCircle(100 + 25*i, 100, 25, point);
+//////            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+////        }
+////        Paint mPaint = new Paint();
+////        mPaint.setAlpha(0);  //设置透明度为0
+////        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN)); //设置两图相交时的模式，那相交处同
+////        mPaint.setAntiAlias(true);//抗锯齿
+////        mPaint.setDither(true);//消除拉动，使画面圓滑
+////        mPaint.setStyle(Paint.Style.STROKE); //设置画笔为空心，否则会是首尾连起来多边形内一块为透明。
+////        mPaint.setStrokeJoin(Paint.Join.ROUND); //结合方式，平滑
+////        mPaint.setStrokeCap(Paint.Cap.ROUND);  //圆头
+////        mPaint.setStrokeWidth(20);//设置空心边框宽
+//
+//        Paint mPaint = new Paint();
+//        mPaint.setAlpha(0);
+//        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+//        mPaint.setAntiAlias(true);
+//        canvas.drawCircle(10, 10, 40, mPaint);
+////        Path path = new Path();
+////        path.addCircle(100, 100, 40, Path.Direction.CW);
+////        canvas.drawPath(path, mPaint);
+//
+//        Log.d("MapView", "onDraw()");
     }
 }
