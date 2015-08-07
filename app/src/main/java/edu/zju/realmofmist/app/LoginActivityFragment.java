bda2af102f6be35efa01af08986b5b3c9af495e3
dongxinb <dongxinb@gmail.com>
2015-08-07 14:26:39 +0800

    update

--- app/src/main/java/edu/zju/realmofmist/app/LoginActivityFragment.java
+++ app/src/main/java/edu/zju/realmofmist/app/LoginActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import cn.smssdk.EventHandler;
 import cn.smssdk.SMSSDK;
 import edu.zju.realmofmist.R;
 
@@ -15,6 +16,26 @@ import edu.zju.realmofmist.R;
 public class LoginActivityFragment extends Fragment {
 
     public LoginActivityFragment() {
+        SMSSDK.initSDK(getActivity(), "96cf4fe49b38", "2f3a5163ca2b1145ee6c4bbafc3b2bcd");
+        EventHandler eventHandler = new EventHandler() {
+            @Override
+            public void afterEvent(int event, int result, Object data) {
+
+                if (result == SMSSDK.RESULT_COMPLETE) {
+                    //回调完成
+                    if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+                        //提交验证码成功
+                    } else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
+                        //获取验证码成功
+                    } else if (event == SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES) {
+                        //返回支持发送验证码的国家列表
+                    }
+                } else {
+                    ((Throwable) data).printStackTrace();
+                }
+            }
+        };
+        SMSSDK.registerEventHandler(eventHandler);
     }
 
     @Override
@@ -22,8 +43,13 @@ public class LoginActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_login, container, false);
-        SMSSDK.initSDK(getActivity(), "96cf4fe49b38", "2f3a5163ca2b1145ee6c4bbafc3b2bcd");
-
+//        SMSSDK.getVerificationCode("86", "18868109810");
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        SMSSDK.unregisterAllEventHandler();
+        super.onDestroy();
     }
 }
